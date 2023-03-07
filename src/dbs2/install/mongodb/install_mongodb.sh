818db01ff70d05d17486b2cde86bea18854fc0ef
Peter Hertkorn <peter.hertkorn@reutlingen-university.de>
2023-03-07 23:47:35 +0100

    removed installation of mongodb-compass due to problems in oracle cloud

--- src/dbs2/install/mongodb/install_mongodb.sh
+++ src/dbs2/install/mongodb/install_mongodb.sh
@@ -15,11 +15,11 @@ apt install -y mongodb-org
 # Step 5 Configure MongoDB
 
 # Compass
-wget https://downloads.mongodb.com/compass/mongodb-compass_1.33.1_amd64.deb
-apt install -y libsecret-1-0 libgconf-2-4 libnss3
-apt install -y ./mongodb-compass_*_amd64.deb
+#wget https://downloads.mongodb.com/compass/mongodb-compass_1.33.1_amd64.deb
+#apt install -y libsecret-1-0 libgconf-2-4 libnss3
+#apt install -y ./mongodb-compass_*_amd64.deb
 
-rm mongodb-compass_1.33.1_amd64.deb
+#rm mongodb-compass_1.33.1_amd64.deb
 
 # Desktop Icon
 cp $INST_SCRIPTS/mongodb/mongodb-compass.desktop /usr/share/applications/
