a673f07d6aaf51b28b0ddfc8633768c4794c5d31
Peter Hertkorn <peter.hertkorn@reutlingen-university.de>
2023-03-08 21:48:22 +0100

    add detached in start script, removed neo4j-desktop installation, update cassandra installation script

--- src/dbs2/install/cassandra/install_cassandra.sh
+++ src/dbs2/install/cassandra/install_cassandra.sh
@@ -5,5 +5,6 @@ wget https://dlcdn.apache.org/cassandra/4.1.0/apache-cassandra-4.1.0-bin.tar.gz
 
 tar -xf apache-cassandra-4.1.0-bin.tar.gz -C /opt/
 chown -R mki.mki /opt/apache-cassandra-4.1.0
+rm apache-cassandra-4.1.0-bin.tar.gz
 
 # Desktop Icon
