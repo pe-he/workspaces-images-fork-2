3d95350e04ad118539b7aaa0ef1b49e335c1a7ac
Peter Hertkorn <peter.hertkorn@reutlingen-university.de>
2023-03-08 19:15:47 +0100

    add volume to start script oracle

--- start_oracle.sh
+++ start_oracle.sh
@@ -5,5 +5,5 @@ docker stop dbs2-workspace
 docker rm dbs2-workspace
  
 # create container
-docker run -itd --name dbs2-workspace --network=dbs2 --shm-size=1024m -p 6901:6901 -v ~/Docker/dbs2/mki:/home/mki -e VNC_PW=dbs2 peterhertkorn/dbs2:develop
+docker run -itd --name dbs2-workspace --network=dbs2 --shm-size=1024m -p 6901:6901 -v ~/Docker/dbs2/mki:/home/mki -v dbs2-opt:/opt -e VNC_PW=dbs2 peterhertkorn/dbs2:develop
 
